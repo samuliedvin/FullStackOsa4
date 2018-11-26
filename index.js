19a2334321594fcd1de8d65150ad0ca5af59439e
Samuli Virtanen <samuliedvin@gmail.com>
2018-11-26 14:33:21 +0200

    Add solutions for exercises 4.1. and 4.2.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.error)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
