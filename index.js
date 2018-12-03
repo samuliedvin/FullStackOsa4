634a81e73a91dd8cf8d17eff92525ebd5fb4025b
Samuli Virtanen <samuliedvin@gmail.com>
2018-12-03 12:38:40 +0200

    Add solution for exercise 4.18.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,6 +29,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.logger)
 app.use(middleware.error)
